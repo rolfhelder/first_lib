82748c10e6224352e017e07bfa4dbac91fa563c5
Rolf Helder <rolfhelder@gmail.com>
2022-06-29 09:38:41 -0300

    add initial files cloned from https://github.com/alura-cursos/2299-lib-nodejs-markdown/tree/aula-1 | add README.md | update index.js with functions to get a local file and extract url link strings

---
+++ README.md
@@ -0,0 +1,38 @@
+<h1>NodeJS Library to extract url links from a markdown file</h1> 
+
+<p align="center">
+  <img src="https://img.shields.io/static/v1?label=NodeJS&message=16.15.1&color=green&style=for-the-badge&logo=typescript"/>
+  <img src="https://img.shields.io/static/v1?label=License&message=MIT&color=blue"/>
+  <img src="https://img.shields.io/static/v1?label=Repo_status&message=WIP&color=red"/>
+</p>
+
+<p align="center">
+  <a href="#project">Project</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+  <a href="#technologies">Technologies</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+  <a href="#license">License</a>
+</p>
+
+<h2 id="project">💻 Project</h2>
+
+<p>This repo is a NodeJS library to extract url links from a markdown file.</p>
+
+<p>Work in progress. DO NOT USE YET.</p>
+
+<p>This is an exercise made as part of a NodeJS course.</p>
+
+<img src="/" alt="No demo yet">
+
+<h2 id="technologies">🧪 Technologies</h2>
+
+<p>Project developed using the following technologies:</p>
+<ul>
+    <li>NodeJS</li>    
+</ul>
+
+<h2 id="license">📝 License</h2>
+
+<p>This project uses MIT license. See <a href="https://en.wikipedia.org/wiki/MIT_License">LICENSE</a> for more details.</p> 
+
+<hr>
+
+<footer align="center">Made without ☕ by Rolf Helder</footer>
--- index.js
+++ index.js
@@ -1,11 +1,73 @@
-const chalk = require('chalk');
+import { match } from 'assert';
+import chalk from 'chalk';
+import fs from 'fs';
+console.log(chalk.blue('Begin!'));
 
-console.log(chalk.blue('vamos começar!'));
+/*// getLinks with text.match = returns an array with the complete string
+function getLinks(text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^\)]*)\)/gm;
+  const links = text.match(regex);
+  console.log(chalk.blue(links));
+}
+*/
 
-const paragrafo = 'Texto retornado por uma função';
 
-function texto(string) {
-  return string;
+// getLinks with regex.exec
+function getLinks(text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^\)]*)\)/gm;
+  const resultArray = [];
+  let temp
+
+  while ((temp = regex.exec(text)) !== null) {
+    
+    resultArray.push({ [temp[1]]: temp[2]})
+  }
+
+  return resultArray;
 }
 
-console.log(texto(paragrafo));
+function processError(err) {
+  throw err = new Error(chalk.redBright(err.code,'!!!file path error!!!'));
+}
+
+//getFile async/await 
+async function getFile(filePath) {
+  const encoding = 'utf-8';
+  
+  try {
+  const data = await fs.promises.readFile(filePath, encoding)
+  console.log(getLinks(data));  
+    
+  } catch(err){
+    processError(err);
+  } finally{
+    console.log('\nProgram finalized');
+  }
+}
+/*//getFile async promises.readfile
+function getFile(filePath) {
+  const encoding = 'utf-8';
+  
+  fs.promises
+    .readFile(filePath, encoding)
+    .then(data => console.log(chalk.green(data)))
+    .catch((err) => processError(err))
+  }
+*/
+
+/*//getFile synchronous version
+function getFile(filePath) {
+  const encoding = 'utf-8';
+  // readFile(file, [encoding], [callback]);
+  fs.readFile(filePath, encoding , (err,data) => {
+
+    if(err){
+      processError(err);
+    }
+
+    console.log(chalk.green(data));
+  })
+}
+*/
+
+getFile('./files/text1.md');
